7169fb5f55ae9d362017e53d12e9e91ddfb8f361
Kaio Silva <kaiohp.silva1@gmail.com>
2024-10-17 14:40:30 -0300

    feat: add error handler

---
+++ src/erros/error_handler.py
@@ -0,0 +1,26 @@
+from http import HTTPStatus
+
+from src.erros.types import (
+    HttpBadRequestError,
+    HttpInternalServerError,
+    HttpNotFoundError,
+)
+from src.presentation.http_types.http_response import HttpResponse
+
+
+def handle_error(error: Exception) -> HttpResponse:
+    if isinstance(
+        error,
+        (HttpBadRequestError, HttpNotFoundError, HttpInternalServerError),
+    ):
+        return HttpResponse(
+            status_code=error.status_code,
+            body={'errors': {'title': error.name, 'detail': error.message}},
+        )
+
+    return HttpResponse(
+        status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+        body={
+            'errors': {'title': 'Internal Server Error', 'detail': str(error)}
+        },
+    )
