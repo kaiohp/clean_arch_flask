0559f6ef6feb3f0dda6e17a9f5a4feb280100451
Kaio Silva <kaiohp.silva1@gmail.com>
2024-10-17 14:40:37 -0300

    feat: add validators to routes

--- src/main/routes/routes.py
+++ src/main/routes/routes.py
@@ -4,6 +4,8 @@ from src.erros.error_handler import handle_error
 from src.main.adapters.request_adapter import request_adapter
 from src.main.composers.user_finder_compose import user_finder_compose
 from src.main.composers.user_register_compose import user_register_compose
+from src.validator.user_finder_validator import user_finder_validator
+from src.validator.user_register_validator import user_register_validator
 
 user_routes_bp = Blueprint('user_routes', __name__)
 
@@ -11,6 +13,7 @@ user_routes_bp = Blueprint('user_routes', __name__)
 @user_routes_bp.route('/user/find', methods=['GET'])
 def find_user():
     try:
+        user_finder_validator(request)
         http_response = request_adapter(request, user_finder_compose())
     except Exception as error:
         http_response = handle_error(error)
@@ -21,6 +24,7 @@ def find_user():
 @user_routes_bp.route('/user', methods=['POST'])
 def register_user():
     try:
+        user_register_validator(request)
         http_response = request_adapter(request, user_register_compose())
     except Exception as error:
         http_response = handle_error(error)
