6747adf694437e52965f11599f6d8952bf934652
Kaio Silva <kaiohp.silva1@gmail.com>
2024-10-17 14:40:33 -0300

    feat: add error handler to routes

--- src/main/routes/routes.py
+++ src/main/routes/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 
+from src.erros.error_handler import handle_error
 from src.main.adapters.request_adapter import request_adapter
 from src.main.composers.user_finder_compose import user_finder_compose
 from src.main.composers.user_register_compose import user_register_compose
@@ -9,13 +10,19 @@ user_routes_bp = Blueprint('user_routes', __name__)
 
 @user_routes_bp.route('/user/find', methods=['GET'])
 def find_user():
-    http_response = request_adapter(request, user_finder_compose())
+    try:
+        http_response = request_adapter(request, user_finder_compose())
+    except Exception as error:
+        http_response = handle_error(error)
 
     return jsonify(http_response.status_code, http_response.body)
 
 
 @user_routes_bp.route('/user', methods=['POST'])
 def register_user():
-    http_response = request_adapter(request, user_register_compose())
+    try:
+        http_response = request_adapter(request, user_register_compose())
+    except Exception as error:
+        http_response = handle_error(error)
 
     return jsonify(http_response.status_code, http_response.body)
