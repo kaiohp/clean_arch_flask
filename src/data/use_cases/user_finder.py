c5744388e6da386a1255c44da7a52fa2616434e5
Kaio Silva <kaiohp.silva1@gmail.com>
2024-10-17 14:40:20 -0300

    refactor: renamed domain/usecases to domain/use_cases

--- src/data/use_cases/user_finder.py
+++ src/data/use_cases/user_finder.py
@@ -1,27 +1,47 @@
 from src.data.interfaces.users_repository import UsersRepositoryInterface
-from src.domain.usecases.user_finder import UserFinder as UserFinderInterface
+from src.domain.models.users import Users
+from src.domain.use_cases.user_finder import UserFinder as UserFinderInterface
 
 
 class UserFinder(UserFinderInterface):
-    FIRST_NAME_MAX_LENGHT = 18
-
     def __init__(self, users_repository: UsersRepositoryInterface) -> None:
         self.__users_repository = users_repository
 
     def find(self, first_name: str) -> dict:
+        self.__validate_first_name(first_name)
+
+        users = self.__search_user_by_first_name(first_name)
+
+        response = self.__format_response(users)
+
+        return response
+
+    @classmethod
+    def __validate_first_name(cls, first_name: str) -> None:
+        FIRST_NAME_MAX_LENGHT = 18
+
         if not first_name.isalpha():
             raise Exception('Invalid first name to find')
 
-        if len(first_name) > UserFinder.FIRST_NAME_MAX_LENGHT:
+        if len(first_name) > FIRST_NAME_MAX_LENGHT:
             raise Exception(
-                'First name exceed the 18 characteres maximo lenght'
+                'First name exceed the 18 characteres maximum lenght'
             )
 
+    def __search_user_by_first_name(self, first_name: str) -> list:
         users = self.__users_repository.get_user_by_first_name(first_name)
-
         if not users:
             raise Exception('User dont find.')
+        return users
 
-        response = {'type': 'Users', 'count': len(users), 'attributes': users}
-
-        return response
+    @classmethod
+    def __format_response(cls, users: list[Users]) -> dict:
+        attributes = []
+        for user in users:
+            record = {
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'birth_date': user.birth_date,
+            }
+            attributes.append(record)
+        return {'type': 'Users', 'count': len(users), 'attributes': attributes}
